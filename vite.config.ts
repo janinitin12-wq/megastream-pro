f8d1f94063ead51124553708c1e37a10d4c8a35e
janinitin12-wq <janinitin12@gmail.com>
2026-01-22 17:35:22 +0530

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import process from 'node:process';
 
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.API_KEY),
+    },
+    build: {
+      outDir: 'dist',
+      sourcemap: false
+    },
+    server: {
+      port: 3000,
+    }
+  };
 });
